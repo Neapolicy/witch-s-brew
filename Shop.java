e8fa7d6ddd9df868930690921357e7fdba09bfd3
Neapolicy <matthewlin559@gmail.com>
2023-11-14 08:42:17 -0500

    got started on shop

--- Shop.java
+++ Shop.java
@@ -1,53 +1,56 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Shop {
-    private int balance = 499;
+    private int balance = 4999;
     private Scanner s = new Scanner(System.in);
     private String answer;
     private ArrayList<String> goods = new ArrayList<>();
-    private ArrayList<String> bought = new ArrayList<String>(); //keeps track of bought good
-    public Shop()
-    {
+    private ArrayList<String> bought = new ArrayList<String>(); // keeps track of bought good
+
+    public Shop() {
         addItems();
     }
-    public void goods()
-    {
+
+    public void goods() {
         System.out.println("What would you like to buy?\nYou have " + balance + " shells\nType -1 to exit");
-        for (int i = 0; i < goods.size(); i++)
-        {
+        for (int i = 0; i < goods.size(); i++) {
             System.out.println(i + 1 + ": " + goods.get(i));
         }
-        answer = s.nextLine().toLowerCase();
+        answer = getUserInput();
         buyItems(answer);
     }
-    public int findItem(String item)
-    {
-        for (int i = 0; i < goods.size(); i++)
-        {
-            if (goods.contains(item))
-            {
+
+    private String getUserInput() {
+        System.out.print("Enter your choice: ");
+        return s.nextLine().toLowerCase();
+    }
+
+    public int findItem(String item) {
+        for (int i = 0; i < goods.size(); i++) {
+            if (goods.get(i).contains(item)) {
                 System.out.println(i);
                 return i;
             }
         }
-        return 0;
+        return -1; // Item not found
     }
 
-    public void priceComparison(int cost, String item)
-    {
-        if (balance >= cost)
-        {
+    public void priceComparison(int cost, String item) {
+        if (balance >= cost) {
             balance -= cost;
             System.out.println("You have successfully bought " + item);
-            goods.remove(findItem(item));
+            int index = findItem(item);
+            if (index != -1) {
+                goods.remove(index);
+            }
             goods();
-        }
-        else
-        {
+        } else {
             System.out.println("Invalid, please try again\n");
             goods();
         }
     }
+
     public void buyItems(String res) {
         switch (res) {
             case "sword", "1" -> priceComparison(20, "Sword");
@@ -58,23 +61,24 @@ public class Shop {
             case "surgeons toolkit", "6" -> priceComparison(40, "Surgeons Toolkit");
             case "smoke bomb", "7" -> priceComparison(60, "Smoke Bomb");
             case "powdered wig", "8" -> priceComparison(500, "Powdered Wig");
-            case "-1" -> {}
+            case "-1" -> {
+            }
             default -> {
                 System.out.println("Invalid choice, please try again\n");
                 goods();
             }
         }
     }
-    public void setBalance(int balance)
-    {
+
+    public void setBalance(int balance) {
         this.balance = balance;
     }
-    public void addItems()
-    {
+
+    public void addItems() {
         goods.add("Sword (Weapon) - 20 shells");
-        goods.add("Flintlock (Weapon) - 50 shells");
+        goods.add("Flintlock (Weapon) - 70 shells");
         goods.add("Musket (Weapon) - 100 shells");
-        goods.add("Mechanical Arm (Accessory) - 35 shells"); // the ones with the weird and sucky names are placeholders
+        goods.add("Mechanical Arm (Accessory) - 35 shells");
         goods.add("Stronger Steel (Accessory) - 30 shells");
         goods.add("Surgeons Toolkit (Accessory) - 40 shells");
         goods.add("Smoke Bomb (Accessory) - 60 shells");
