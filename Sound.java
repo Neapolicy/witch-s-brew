9534a06113907a8945b516b6c9ef977ac8b79d54
neapolicy <matthewlin559@gmail.com>
2023-11-16 15:18:27 -0500

    sound player

---
+++ Sound.java
@@ -0,0 +1,31 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound
+{
+
+    public void sound(String name) { // code found on stack overflow
+        // (https://stackoverflow.com/questions/69870550/java-music-file-returns-no-such-file-or-directory-but-path-seems-correct)
+        try {
+
+            System.out.println("music is now playing");
+            File file = new File("Sound/Music/" + name + ".wav");
+            AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(stream);
+            clip.start();
+
+            // sleep to allow enough time for the clip to play
+            Thread.sleep(10);
+
+            stream.close();
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
