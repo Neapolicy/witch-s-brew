5494292f5aba35748bcdb6863fc4e7503e1fff26
Neapolicy <matthewlin559@gmail.com>
2023-11-16 21:08:23 -0500

    altered how stock market works

--- Lobby.java
+++ Lobby.java
@@ -21,57 +21,71 @@ public class Lobby {
     }
 
     public void decisionMaker() {
-        dayCount();
-        System.out.println("\nWhat would " + name + " like to do?\n");
-        System.out.println("Lower this island's security (1)");
-        System.out.println("Invest in the stock market (2)");
-        System.out.println("Access the shop (3)");
-        System.out.println("Check your inventory (4)");
-        System.out.println("Edit character (5)");
-        System.out.println("(You currently have " + balance + " shells");
-        answer = s.nextLine();
-        turns -= 1;
-        switch (answer) {
-            case "1":
-                decisionMaker();
-            case "2":
-                stockMarket.setCash(balance);
-                balance = stockMarket.cashBack();
-                updateBalance();
-                decisionMaker();
-            case "3":
-                shop.setBalance(balance);
-                shop.goods();
-                decisionMaker();
-            case "4":
-                boughtStuff = shop.getItemsBought();
-                if (boughtStuff.size() == 0) {
-                    System.out.println("Nothing here...");
-                } else {
-                    System.out.println("This is your inventory");
-                    for (int i = 0; i < boughtStuff.size(); i++) {
-                        System.out.println(boughtStuff.size());
+        while (turns != 0 && days != 7) {
+            System.out.println("\nWhat would " + name + " like to do?\n");
+            System.out.println("Lower this island's security (1)");
+            System.out.println("Invest in the stock market (2)");
+            System.out.println("Access the shop (3)");
+            System.out.println("Check your inventory (4)");
+            System.out.println("Edit character (5)");
+            System.out.println("(You currently have " + balance + " shells");
+            System.out.println(turns); // remember to remove this in the final version
+            answer = s.nextLine();
+            switch (answer) {
+                case "1":
+                    turns -= 1;
+                    break;
+                case "2":
+                    stockMarket.setCash(balance);
+                    balance = stockMarket.cashBack();
+                    updateBalance();
+                    turns -= 1;
+                    break;
+                case "3":
+                    shop.setBalance(balance);
+                    shop.goods();
+                    break;
+                case "4":
+                    boughtStuff = shop.getItemsBought();
+                    if (boughtStuff.size() == 0) {
+                        System.out.println("Nothing here...");
+                    } else {
+                        System.out.println("This is your inventory");
+                        for (int i = 0; i < boughtStuff.size(); i++) {
+                            System.out.println(boughtStuff.size());
+                        }
                     }
-                }
-                decisionMaker();
-            default:
-                decisionMaker();
+                    break;
+                default:
+                    break;
+            }
         }
-    }
-    public boolean turnZero()
-    {
-        return turns == 0;
+        dayCheck();
     }
 
     public void dayCount()
     {
-        if (turnZero())
+        stockMarket.cashFlow();
+        System.out.println("day pass");
+        days += 1;
+        turns = 2;
+        decisionMaker();
+    }
+    public void dayCheck()
+    {
+        if (days == 7)
         {
-            stockMarket.cashFlow();
-            System.out.println("day pass");
-            days += 1;
-            turns = 2;
+            bossRaidOni();
         }
+        else
+        {
+            dayCount();
+        }
+    }
+
+    private void bossRaidOni()
+    {
+
     }
 
     public void updateBalance() {
