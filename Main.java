aeae6a82751f77688894f2086010368a8c5ec573
Neapolicy <matthewlin559@gmail.com>
2023-11-18 17:22:59 -0500

    modifications (reminder to myself to alter their stats)

--- Main.java
+++ Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         //printSlow("You start here, in an unknown land, after the CEO of Racism\nWhat could go wrong?\n");
-        Lobby lobby = new Lobby();
-        Sound sound  = new Sound();
-        for (int i = 0; i < 1; i++)
-        {
-            sound.sound("Gun_Load", 400);
-            sound.sound("Gun_Fire", 800);
-            sound.sound("Gun_Load", 400);
-            sound.sound("Gun_Fire", 800);
-        }
+        /*Lobby lobby = new Lobby();*/
+        Sound sound = new Sound();
+        sound.sound("Gun_Load", 400);
+        sound.sound("Gun_Fire", 800);
+        sound.sound("Gun_Load", 400);
+        sound.sound("Gun_Fire", 800);
     }
 
-    private static void printSlow(String text)
-    {
+    private static void printSlow(String text) {
         for (int i = 0; i < text.length(); i++) {
             System.out.print(text.charAt(i));
             try {
